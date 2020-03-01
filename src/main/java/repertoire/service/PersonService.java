95841e0590fb6673002c88a43aacf19eb0b6d122
AV <alexandre.verept@isen.yncrea.fr>
2020-03-01 11:44:20 +0100

    Change for to iterator

--- src/main/java/repertoire/service/PersonService.java
+++ src/main/java/repertoire/service/PersonService.java
@@ -4,6 +4,7 @@ import repertoire.dao.PersonDao;
 import repertoire.entities.Person;
 
 import java.time.LocalDate;
+import java.util.Iterator;
 import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -21,8 +22,11 @@ public class PersonService {
 		
 		PersonDao personDao = new PersonDao();
 		List<Person> personsInTheDB = personDao.listPersons();
-		for (int i = 0; i < personsInTheDB.size(); i++) {
-			persons.add(personsInTheDB.get(i));
+		
+		Iterator<Person> iter = personsInTheDB.iterator();
+		
+		while(iter.hasNext()) {
+			persons.add(iter.next());
 		}
 		
 	}
@@ -34,8 +38,10 @@ public class PersonService {
 		PersonDao personDao = new PersonDao();
 		List<Person> personsInTheDB = personDao.listPersons();
 		
-		for (int i = 0; i < personsInTheDB.size(); i++) {
-			PersonServiceHolder.INSTANCE.persons.add(personsInTheDB.get(i));
+		Iterator<Person> iter = personsInTheDB.iterator();
+		
+		while(iter.hasNext()) {
+			PersonServiceHolder.INSTANCE.persons.add(iter.next());
 		}
 		
 		return PersonServiceHolder.INSTANCE.persons;
