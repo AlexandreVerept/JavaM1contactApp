0fb717d6ac7df194036d459cd804154a504f889c
Gabriel <takedadesmullier@gmail.com>
2020-03-13 14:29:05 +0100

    Before Import
    before de l'import

--- src/test/java/repertoire/io/ImportTestCase.java
+++ src/test/java/repertoire/io/ImportTestCase.java
@@ -3,16 +3,43 @@ package repertoire.io;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import repertoire.entities.Person;
 
 public class ImportTestCase {
+	
+	    @Before
+	    public void initfiles() throws IOException {
+	    	Path newPath= Paths.get("src\\main\\resources\\repertoire\\test\\Vcard\\");
+	    	List<String> newList=new ArrayList();
+	    	newList.add("BEGIN:VCARD");
+	    	newList.add("VERSION:2.1");
+	    	newList.add("N:DeGaulle;Charles;Charly Frenchy");
+	    	newList.add("TEL:TYPE=cell:3630");
+	    	newList.add("ADR:TYPE=home:1 rue du Général-de-Gaulle 52330 Colombey-les-deux-eglises");
+	    	newList.add("EMAIL:FranceLibre@gmail.com");
+	    	newList.add("ANNIVERSARY:1890-11-22");
+	    	newList.add("END:VCARD");
+	    	Path filePath=newPath.resolve("1945DeGaulleCharles.vcard");
+	    	if(Files.notExists(filePath)) {
+	    	Files.createFile(newPath.resolve("1945DeGaulleCharles.vcard"));
+	    	Files.write(newPath.resolve("1945DeGaulleCharles.vcard"),newList,StandardCharsets.UTF_8);}
+	    	
+	    	filePath=newPath.resolve("1945DeGaulleCharles.txt");
+	    	if(Files.notExists(filePath)) {
+		    	Files.createFile(filePath);}
+	    	
+	    }
+	
 
 	@Before
 	public void initfiles() {
