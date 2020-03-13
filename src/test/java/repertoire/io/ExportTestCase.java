d8ce2be2cc1fb4126148dd34134b30121dd059ee
AV <alexandre.verept@isen.yncrea.fr>
2020-03-13 11:47:12 +0100

    Before statement
    Add @before to the unit test import

--- src/test/java/repertoire/io/ExportTestCase.java
+++ src/test/java/repertoire/io/ExportTestCase.java
@@ -1,30 +1,39 @@
-/**
- * 
- */
 package repertoire.io;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
+import org.junit.Before;
 import org.junit.Test;
 import repertoire.entities.Person;
 
 /**
- * @author Gabriel
- *
+ * @authors Gabriel Desmullier, Daniel Gheyssens, Alexandre Verept
  */
+
 public class ExportTestCase {
+	
+	/**
+	 * This methods is used to clear the test folder before each and every test.
+	 */
+	@Before
+	public void deleteVcardIfAlreadyExists() {
+		Path filePath = Paths.get("src\\main\\resources\\repertoire\\test\\Vcard");
+		File file = new File(filePath.toString());
+		if(file.exists() && !file.isDirectory()) { 
+		    file.delete();
+		}
+	}
 
 	/**
-	 * 
+	 * test if the file is created
 	 */
-
-	
-    //test si fichier est bien créé
 	@Test
 	public void shouldAddVcard() throws IOException {
 		Export testExport=new Export("src\\main\\resources\\repertoire\\test\\Vcard");
